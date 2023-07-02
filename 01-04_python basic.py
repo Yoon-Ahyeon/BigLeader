4b73a2a4a9bf8911d5f70d7a9489456f17b5ae9b
윤아현 <74540582+Yoon-Ahyeon@users.noreply.github.com>
2023-07-03 00:09:44 +0900

    Update 01-04_python basic.py

--- 01-04_python basic.py
+++ 01-04_python basic.py
@@ -35,66 +35,57 @@ for i in range(ROW):
 
 Display()
 
-# ## Image processing !!!
-#
-# ## (1) 영상을 50 밝게 처리하자. (숫자 더하기)
-# for i in range(ROW):
-#     for k in range(COL):
-#         # 먼저 확인을 안하면, 오버플로우가 발생하지 않아도 프로그램이 죽을 수도 있다.
-#         if(image[i][k] + 50 > 255):
-#             image[i][k] = 255
-#         else:
-#             image[i][k] += 50
-# Display()
-#
-# ## (2) 영상을 100 어둡게 처리하자. (숫자 빼기)
-# for i in range(ROW):
-#     for k in range(COL):
-#         if(image[i][k] - 100 < 0):
-#             image[i][k] = 0
-#         else:
-#             image[i][k] -= 100
-#
-# Display()
+## Image processing !!!
+
+## (1) 영상을 50 밝게 처리하자. (숫자 더하기)
+for i in range(ROW):
+    for k in range(COL):
+        # 먼저 확인을 안하면, 오버플로우가 발생하지 않아도 프로그램이 죽을 수도 있다.
+        if(image[i][k] + 50 > 255):
+             image[i][k] = 255
+         else:
+             image[i][k] += 50
+Display()
+
+## (2) 영상을 100 어둡게 처리하자. (숫자 빼기)
+for i in range(ROW):
+    for k in range(COL):
+         if(image[i][k] - 100 < 0):
+             image[i][k] = 0
+         else:
+             image[i][k] -= 100
+
+Display()
 
 ## (3) 완전 흑백 처리
 
 # 중앙값을 통해 완전 흑백 처리 가능 (큰 의미는 없다) - 이상한 값이 나올 수 있기 때문에 안전상으로 수행하기
-# hap = 0
-# avg = 0 #참조오류때문에 사용하는 것이 좋다.
-# for i in range(ROW):
-#     for k in range(COL):
-#         hap += image[i][k]
-# avg = hap / (ROW*COL)
-#
-# for i in range(ROW):
-#     for k in range(COL):
-#         if(image[i][k] >= avg):
-#             image[i][k] = 255
-#         else:
-#             image[i][k] = 0
-#
-# Display()
+hap = 0
+avg = 0 #참조오류때문에 사용하는 것이 좋다.
+for i in range(ROW):
+    for k in range(COL):
+        hap += image[i][k]
+avg = hap / (ROW*COL)
+for i in range(ROW):
+    for k in range(COL):
+        if(image[i][k] >= avg):
+            image[i][k] = 255
+        else:
+            image[i][k] = 0
+
+Display()
 
 # 128을 기준으로 반 나누어서 흑백 처리하기
-# for i in range(ROW):
-#     for k in range(COL):
-#         if(image[i][k] >= 128): #128이든, 127이든 상관 없다.
-# #             image[i][k] = 255
-# #         else:
-# #             image[i][k] = 0
-#
-# Display()
+for i in range(ROW):
+    for k in range(COL):
+        if(image[i][k] >= 128): #128이든, 127이든 상관 없다.
+            image[i][k] = 255
+        else:
+            image[i][k] = 0
 
-# ## (4) 반전 처리
-# 밑에가 제대로 된 구현
-# for i in range(ROW):
-#     for k in range(COL):
-#         if(image[i][k] - 255 < 0 ):
-#             image[i][k] = (image[i][k] - 255) * (-1)
-#         else:
-#             image[i][k] = image[i][k] - 255
+Display()
 
+## (4) 반전 처리
 for i in range(ROW):
     for k in range(COL):
         image[i][k] = 255-image[i][k]
